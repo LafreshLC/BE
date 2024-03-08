324ce5f5500e58695a2bccafac3418f4a4084d63
netojaycee <117773169+netojaycee@users.noreply.github.com>
2024-03-08 20:02:01 +0100

    update

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -3,13 +3,8 @@ import { addProduct, allProduct, removeProduct, updateProduct } from "#/controll
 
 const router = Router();
 router.post('/create-product', addProduct);
-<<<<<<< HEAD
 router.patch('/:productId', updateProduct);
 router.get('/all-product', allProduct);
-=======
-router.post('/update-product', updateProduct);
-router.get('/all-product', allProduct); 
->>>>>>> 8521235a2525e1a7d51ef0cb3919a9c978a29f3d
 router.delete('/delete-product', removeProduct); 
 
 export default router 
