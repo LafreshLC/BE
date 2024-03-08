8d2cdb0d37607fdb6ae3704eddcfd014bbe3031f
Ajayi favour <ajayifavour81@gmail.com>
2024-03-08 20:22:12 +0100

    delete product

--- src/routers/product.ts
+++ src/routers/product.ts
@@ -5,6 +5,6 @@ const router = Router();
 router.post('/create-product', addProduct);
 router.patch('/:productId', updateProduct);
 router.get('/all-product', allProduct);
-router.delete('/delete-product', removeProduct); 
+router.delete('/:productId', removeProduct); 
 
 export default router 
